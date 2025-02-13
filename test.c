69d8107e84608dafaf1aee337d24fcbe28da8b4e
Alain Felger <alain.felger93@googlemail.com>
2025-02-13 19:46:29 +0100

    some stuff

---
+++ test.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+#include <stdlib.h>
+
+void keep_me(void *ptr)
+{
+	static 
+}
+
+int main()
+{
+	while (1)
+	{
+		printf("Ask me");
+		char *test = readline("something: ");
+		add_history(test);
+		printf("\n>%s\n", test);
+		// free(test);
+	}
+	return (0);
+}
