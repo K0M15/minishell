d85ca5a499aa1789dd95ef7b0f525873b3efd7dc
Alain Felger <alain.felger93@googlemail.com>
2025-02-18 15:34:10 +0100

    added signals to program and all the other builtins + ascii art

--- src/minishell.c
+++ src/minishell.c
@@ -6,28 +6,27 @@
 /*   By: afelger <afelger@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/06 17:59:31 by afelger           #+#    #+#             */
-/*   Updated: 2025/02/14 16:08:10 by afelger          ###   ########.fr       */
+/*   Updated: 2025/02/18 15:27:24 by afelger          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+
 #include "minishell.h"
 
-
-
+volatile sig_atomic_t g_ms_signal;
 
 int main(int argc, char **argv, char **envp)
 {
 	(void) argc;
 	(void) argv;
-	import_env(envp);
-	printf("%s\n", get_env("USER"));
-	printf("%s\n", get_env("LANG"));
-	printf("%s\n", get_env("PATH"));
-	printf("%s\n", get_env("LOGNAME"));
-	// char *str;
-	// while (1)
-	// {
-	// 	str = readline(">");
-	// 	free(str);
-	// }
+	(void) envp;
+
+	ms_env_init();
+	ms_display_welcome();
+	char *str;
+	while (1)
+	{
+		str = readline(ms_get_prompt());
+		free(str);
+	}
 }
