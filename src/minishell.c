6c2b0a7748932f1bf5b2fb16fbf9501911a89ca8
Alain Felger <alain.felger93@googlemail.com>
2025-02-19 16:27:21 +0100

    tests with history

--- src/minishell.c
+++ src/minishell.c
@@ -6,7 +6,7 @@
 /*   By: afelger <afelger@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/06 17:59:31 by afelger           #+#    #+#             */
-/*   Updated: 2025/02/18 15:27:24 by afelger          ###   ########.fr       */
+/*   Updated: 2025/02/19 15:55:18 by afelger          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,26 @@
 #include "minishell.h"
 
 volatile sig_atomic_t g_ms_signal;
+
+void write_hist_entry(int fd, HIST_ENTRY	*entry)
+{
+	if (entry == NULL)
+		return;
+	if (write(fd, ) == -1)
+		printf("ERR: FD closed");
+}
+
+void dump_history()
+{
+	HISTORY_STATE *state;
+	int i;
+
+	state = history_get_history_state();
+	i = -1;
+	printf("\n");
+	while (++i <= state->offset)
+		printf("%d: < %ld > %s\n", i, (long) state->entries[i]->timestamp, state->entries[i]->line);
+}
 
 int main(int argc, char **argv, char **envp)
 {
@@ -23,10 +43,16 @@ int main(int argc, char **argv, char **envp)
 
 	ms_env_init();
 	ms_display_welcome();
+	using_history();
 	char *str;
 	while (1)
 	{
-		str = readline(ms_get_prompt());
-		free(str);
+		str = ft_mem_reg(readline(ms_get_prompt()));
+		rl_replace_line("something", 2);
+		add_history(str); // check beforehand if string is empty
+		if (ft_strncmp("exit", str, 4) == 0)
+			break;
 	}
+	dump_history();
+	cleanup(0);
 }
