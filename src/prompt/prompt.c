84ed8af5166f33899e2f5363097284370f546b08
Alain Felger <alain.felger93@googlemail.com>
2025-02-19 16:26:59 +0100

    some color magic, lets see how this turns out

--- src/prompt/prompt.c
+++ src/prompt/prompt.c
@@ -6,29 +6,34 @@
 /*   By: afelger <afelger@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/16 14:13:02 by afelger           #+#    #+#             */
-/*   Updated: 2025/02/18 15:27:57 by afelger          ###   ########.fr       */
+/*   Updated: 2025/02/19 14:33:26 by afelger          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+#include <curses.h>
 
 
 #define MS_WELC_0 ",--.   ,--,--,--.  ,--,--.,---. ,--.  ,--,------,--.  ,--.   "
 #define MS_WELC_1 "|   `.'   |  |  ,'.|  |  '   .-'|  '--'  |  .---|  |  |  |   "
-#define MS_WELC_2 "|  |'.'|  |  |  |' '  |  `.  `-.|  .--.  |  `--,|  |  |  |   "
+#define MS_WELC_2 "|by|'.'|LI|MI| &|' ' A|LI`.  `-.|  .--.  |  `--,|  |  |  |   "
 #define MS_WELC_3 "|  |   |  |  |  | `   |  .-'    |  |  |  |  `---|  '--|  '--."
 #define MS_WELC_4 "`--'   `--`--`--'  `--`--`-----'`--'  `--`------`-----`-----'"
 
-#define SET_COLOR_VIOLET "\e[48;5;057m"
+#define SET_BGCOLOR_ONE "\e[48;5;058m"
+#define SET_BGCOLOR_TWO "\e[48;5;059m"
+#define SET_BGCOLOR_THREE "\e[48;5;060m"
+#define SET_BGCOLOR_FOUR "\e[48;5;061m"
+#define SET_BGCOLOR_FIVE "\e[48;5;062m"
 #define RESET_COLOR "\e[0m"
 
 void	ms_display_welcome(void)
 {
-	printf("%s%s%s\n", SET_COLOR_VIOLET, MS_WELC_0, RESET_COLOR);
-	printf("%s%s%s\n", SET_COLOR_VIOLET, MS_WELC_1, RESET_COLOR);
-	printf("%s%s%s\n", SET_COLOR_VIOLET, MS_WELC_2, RESET_COLOR);
-	printf("%s%s%s\n", SET_COLOR_VIOLET, MS_WELC_3, RESET_COLOR);
-	printf("%s%s%s\n", SET_COLOR_VIOLET, MS_WELC_4, RESET_COLOR);
+	printf("%s%s%s\n", SET_BGCOLOR_ONE, MS_WELC_0, RESET_COLOR);
+	printf("%s%s%s\n", SET_BGCOLOR_TWO, MS_WELC_1, RESET_COLOR);
+	printf("%s%s%s\n", SET_BGCOLOR_THREE, MS_WELC_2, RESET_COLOR);
+	printf("%s%s%s\n", SET_BGCOLOR_FOUR, MS_WELC_3, RESET_COLOR);
+	printf("%s%s%s\n", SET_BGCOLOR_FIVE, MS_WELC_4, RESET_COLOR);
 }
 
 char	*ms_get_prompt(void)
@@ -41,5 +46,5 @@ char	*ms_get_prompt(void)
 
 	// pwd = get_appstate()->working_directory;
 	//get start of last directory. if root + 1 display / in prompt
-	return ("minishell >");
+	return (SET_BGCOLOR_FIVE"minishell >"RESET_COLOR);
 }
