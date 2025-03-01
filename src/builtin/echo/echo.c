5853acbddae094de0c6770f3c352b26e0a6d6989
Alain Felger <alain.felger93@googlemail.com>
2025-03-01 09:55:15 +0100

    changed echo to a working type

--- src/builtin/echo/echo.c
+++ src/builtin/echo/echo.c
@@ -6,29 +6,55 @@
 /*   By: afelger <afelger@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/14 17:30:45 by afelger           #+#    #+#             */
-/*   Updated: 2025/02/20 09:19:53 by afelger          ###   ########.fr       */
+/*   Updated: 2025/03/01 09:55:03 by afelger          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+int get_mode(char *arg)
+{
+	int ctr;
+	int result;
+
+	ctr = 1;
+	result = 0;
+	if (arg[0] == '-')
+	{
+		while(arg[ctr])
+		{
+			if (arg[ctr] == 'n')
+				result = result | 0b1;
+			else
+				return (0);
+			ctr++;
+		}
+	}
+	return (result);
+}
+
 int	builtin_echo(int argc, char **argv)
 {
 	int		ctr;
 	size_t	strl;
-	int		has_minus_n;
+	int		mode;
 
-	(void) argc;	
 	ctr = 1;
 	strl = 0;
-	has_minus_n = argc < 3 && \
-		ft_strncmp("-n", argv[1], 2) && ft_strlen(argv[1]) == 2;
-	if (argc < 2 || has_minus_n)
-		return (0);
-	while (argv[ctr] && argv[ctr + 1])
-		printf("%s ", argv[ctr]);
-	printf("%s", argv[ctr]);
-	if (!has_minus_n)
+	mode = 0;
+	if (argc > 1)
+	{
+		mode = get_mode(argv[1]);
+		if (mode)
+			argc++;
+		if (argc < 2 || mode & 0b1)
+			return (0);
+		while (argv[ctr] && argv[ctr + 1])
+			printf("%s ", argv[ctr]);
+		printf("%s", argv[ctr]);
+
+	}
+	if (!(mode & 0b1))
 		printf("\n");
 	return (0);
 }
