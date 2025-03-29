57725be8cb967401d8c66053a3eacd39d068e2f4
ckrasniq <>
2025-03-29 15:06:16 +0100

    changed the prompt to  firey mini's hell

--- src/prompt/prompt.c
+++ src/prompt/prompt.c
@@ -3,33 +3,35 @@
 /*                                                        :::      ::::::::   */
 /*   prompt.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: afelger <afelger@student.42.fr>            +#+  +:+       +#+        */
+/*   By: ckrasniq <ckrasniq@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/16 14:13:02 by afelger           #+#    #+#             */
-/*   Updated: 2025/03/28 16:08:28 by afelger          ###   ########.fr       */
+/*   Updated: 2025/03/29 15:05:37 by ckrasniq         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 #include <curses.h>
 
-#define MS_WELC_0 ",--.   ,--,--,--.  ,--,--.,---. ,--.  ,--,------,--.\
-  ,--.   "
-#define MS_WELC_1 "|   `.'   |  |  ,'.|  |  '   .-'|  '--'  |  .---|  |\
-  |  |   "
-#define MS_WELC_2 "|  |'.'|  |  |  |' '  |  `.  `-.|  .--.  |  `--,|  |\
-  |  |   "
-#define MS_WELC_3 "|  |   |  |  |  | `   |  .-'    |  |  |  |  `---|  '\
---|  '--."
-#define MS_WELC_4 "`--'   `--`--`--'  `--`--`-----'`--'  `--`------`---\
---`-----'"
 
-#define SET_BGCOLOR_ONE "\e[48;5;058m"
-#define SET_BGCOLOR_TWO "\e[48;5;059m"
-#define SET_BGCOLOR_THREE "\e[48;5;060m"
-#define SET_BGCOLOR_FOUR "\e[48;5;061m"
-#define SET_BGCOLOR_FIVE "\e[48;5;062m"
+#define MS_WELC_0 "  __  __    ___    _  _     ___     (\")     ___             _  _     ___     _       _       "
+#define MS_WELC_1 " |  \\/  |  |_ _|  | \\| |   |_ _|     \\|    / __|     o O O | || |   | __|   | |     | |      "
+#define MS_WELC_2 " | |\\/| |   | |   | .` |    | |            \\__ \\    o      | __ |   | _|    | |__   | |__    "
+#define MS_WELC_3 " |_|__|_|  |___|  |_|\\_|   |___|   _____   |___/   TS__[O] |_||_|   |___|   |____|  |____|   "
+#define MS_WELC_4 " |\"\"\"\"\"\"|_|\"\"\"\"\"|_|\"\"\"\"\"|_|\"\"\"\"|_|      |_|\"\"\"\"\"| {======|_|\"\"\"\"\"|_|\"\"\"\"\"|_|\"\"\"\"\"|_|\"\"\"\"\"|"
+#define MS_WELC_5 " \"`-0-0-'\"`-0-0-'\"`-0-0-'\"`-0-0-'\"`-0-0-'\"`-0-0-'./o--000'\"`-0-0-'\"`-0-0-'\"`-0-0-'\"`-0-0-'"
+
+
+
+#define SET_BGCOLOR_ONE "\e[38;5;229m"
+#define SET_BGCOLOR_TWO "\e[38;5;228m"
+#define SET_BGCOLOR_THREE "\e[38;5;202m"
+#define SET_BGCOLOR_FOUR "\e[38;5;196m"
+#define SET_BGCOLOR_FIVE "\e[48;5;088m"
+#define SET_BGCOLOR_SIX "\e[48;5;052m"
 #define RESET_COLOR "\e[0m"
+
+
 
 void	ms_display_welcome(void)
 {
@@ -38,6 +40,8 @@ void	ms_display_welcome(void)
 	printf("%s%s%s\n", SET_BGCOLOR_THREE, MS_WELC_2, RESET_COLOR);
 	printf("%s%s%s\n", SET_BGCOLOR_FOUR, MS_WELC_3, RESET_COLOR);
 	printf("%s%s%s\n", SET_BGCOLOR_FIVE, MS_WELC_4, RESET_COLOR);
+	printf("%s%s%s\n", SET_BGCOLOR_SIX, MS_WELC_5, RESET_COLOR);
+
 }
 
 char	*ms_get_prompt(void)
